29816509a49fbdaba1f276e3a4b2ba53129cacc3
Petrushenko_Yevhenii <ye.petrushenko@tascombank.com.ua>
2021-05-18 17:52:21 +0300

    add filtering to mat-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 import { AppComponent } from './app.component';
 import { MatTableOneComponent } from './mat-table-one/mat-table-one.component';
-import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {MatSortModule} from "@angular/material/sort";
     BrowserAnimationsModule,
     MatButtonModule,
     MatTableModule,
-    MatSortModule
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mat-table-one/mat-table-one.component.ts
+++ src/app/mat-table-one/mat-table-one.component.ts
@@ -42,4 +42,7 @@ export class MatTableOneComponent implements OnInit, AfterViewInit {
     this.dataSource.sort = this.sort;
   }
 
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+  }
 }
