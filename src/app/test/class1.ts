7a41596155cc90db29fa8a0f85896100bd738467
Petrushenko_Yevhenii <ye.petrushenko@tascombank.com.ua>
2021-05-25 18:11:10 +0300

    added some comments

--- src/app/test/class1.ts
+++ src/app/test/class1.ts
@@ -1,6 +1,9 @@
 /**
+ * 
  * @description
  * new feature TypeScript 4.0
+ * Класс Square показывает новую  возможность "TypeScript 4.0" о том, что компилятор понимает, какого типа поля у класса, несмотря на то, чтро  они определены перед конструктором
+ * @see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-0.html
  */
 
 class Square {
