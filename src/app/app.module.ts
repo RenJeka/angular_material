35ffb31615ab732ea1731e274515b40bf7255c20
Petrushenko_Yevhenii <ye.petrushenko@tascombank.com.ua>
2021-05-25 13:37:11 +0300

    added ng-dynamic-forms (just dependencies)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from "@angular/material/table";
 import { MatSortModule } from "@angular/material/sort";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { DynamicFormsMaterialUIModule } from '@ng-dynamic-forms/ui-material';
 
 import { AppComponent } from './app.component';
 import { MatTableOneComponent } from './mat-table-one/mat-table-one.component';
+import { DynamicFormsOneComponent } from './forms/dynamic-forms-one/dynamic-forms-one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatTableOneComponent
+    MatTableOneComponent,
+    DynamicFormsOneComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +27,9 @@ import { MatTableOneComponent } from './mat-table-one/mat-table-one.component';
     MatTableModule,
     MatSortModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule,
+    DynamicFormsMaterialUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forms/dynamic-forms-one/dynamic-forms-one.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-forms-one',
+  templateUrl: './dynamic-forms-one.component.html',
+  styles: [
+  ]
+})
+export class DynamicFormsOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
